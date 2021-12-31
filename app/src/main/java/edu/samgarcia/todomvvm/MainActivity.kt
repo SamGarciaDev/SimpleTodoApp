4c568cd09ccf7f448016c807d2a6edbcefa751a7
Sam Garcia <samgcbusiness@gmail.com>
2021-12-31 14:16:20 +0100

    🎉 Initial commit.

---
+++ app/src/main/java/edu/samgarcia/todomvvm/MainActivity.kt
@@ -0,0 +1,22 @@
+package edu.samgarcia.todomvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import edu.samgarcia.todomvvm.ui.theme.TodoMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoMVVMTheme {
+
+            }
+        }
+    }
+}
